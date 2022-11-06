3606c67a84945c7868911550b8932fb93bbd5427
Krzysztof <krzysztof.ziemski@sagiton.pl>
2022-11-06 22:06:58 +0100

    clear project with postresql connection

---
+++ api/config/postgresqp.ts
@@ -0,0 +1,7 @@
+export const databaseConfigConnection = {
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: 5432,
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer className="text-3xl text-green-600 p-2">footer</footer>
+    </div>
+  );
 }
