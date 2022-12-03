12c00052cde4466e7e371eade959d99aba10aba1
Krzysztof Ziemski <k.b.ziemski@gmail.com>
2022-12-03 07:57:01 +0000

    version 0.2.1

--- utils/filter.ts
+++ utils/filter.ts
@@ -10,18 +10,7 @@ export const productFilter = (item: Product, phrase: string) => {
     if (name.toLocaleLowerCase().includes(word)) points = +4;
     if (category.toLocaleLowerCase().includes(word)) points = +2;
     if (shortDescription.toLocaleLowerCase().includes(word)) points = +1;
-    if (name.toLocaleLowerCase() === "chwosty - Butelkowa Zieleń") {
-      console.log("name.includes(word)", name.includes(word));
-      console.log("category.includes(word)", category.includes(word));
-      console.log(
-        "shortDescription.includes(word)",
-        shortDescription.includes(word)
-      );
-    }
   });
-  console.log(name);
-  if (name.toLocaleLowerCase() === "Chwosty - Butelkowa Zieleń") {
-    console.log(points);
-  }
+
   return points;
 };
