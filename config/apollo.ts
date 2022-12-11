daefb5642bb0cf26724b4be52bacf9e1c186320f
Krzysztof Ziemski <k.b.ziemski@gmail.com>
2022-12-11 11:29:27 +0100

    version 0.4.0

--- config/apollo.ts
+++ config/apollo.ts
@@ -3,11 +3,15 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: process.env.DATO_CMS_URL,
   cache: new InMemoryCache(),
-  headers: {
-    authorization: `Bearer ${process.env.DATO_CMS_API_KEY}`,
-    "X-Include-Drafts":
-      process.env.NODE_ENV === "development" ? "true" : "false",
-  },
+  headers:
+    process.env.NODE_ENV === "development"
+      ? {
+          authorization: `Bearer ${process.env.DATO_CMS_API_KEY}`,
+          "X-Include-Drafts": "true",
+        }
+      : {
+          authorization: `Bearer ${process.env.DATO_CMS_API_KEY}`,
+        },
 });
 
 export default client;
